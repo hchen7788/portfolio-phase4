c62058802557e6520c4bd420016733418ffa9a4e
HaitongC <haitongchen116@gmail.com>
2023-03-07 22:29:15 -0800

    class & shadow dom set up

--- JS/ButtonCount.js
+++ JS/ButtonCount.js
@@ -1,3 +1,12 @@
-class ButtonCount{
-    
+class ButtonCount extends HTMLElement{
+    constructor(){
+        //always call super() first in the constructor
+        super();
+        //attach the Shadow DOM to your web component to protect your scope
+        this.attachShadow({ mode: 'open'});
+        //add your content to the root of the Shadow DOM that was created
+        this.shadowRoot.innerHTML = '<h1>web component</h1>';
+    }
 }
+
+window.customElements.define('button-count', ButtonCount);
