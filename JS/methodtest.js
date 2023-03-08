00f34626e18828143ed06880403ab0f468a6c6c1
HaitongC <haitongchen116@gmail.com>
2023-03-07 21:39:27 -0800

    put implemented

--- JS/methodtest.js
+++ JS/methodtest.js
@@ -29,10 +29,6 @@ function postWithXML(){
     let body = document.getElementById('articleBodyInput').value;
     let date = new Date();
 
-    console.log(name)
-    console.log(body)
-    console.log(date)
-
     let data = `{
         "article-name": ${name},
         "article-body": ${body},
@@ -107,7 +103,63 @@ function getWithFetch(){
 
 
 function put(){
-    
+    let putMethod = document.getElementsByName('putMethod');
+
+    if(putMethod[0].checked){
+        putWithXML();
+    } else if(putMethod[1].checked){
+        putWithFetch();
+    } else{
+        document.getElementById('response').innerText = "Please select a PUT method";
+    }
+}
+
+function putWithXML(){
+    let xhr = new XMLHttpRequest();
+    xhr.open("PUT", "https://httpbin.org/put");
+    xhr.setRequestHeader('Content-Type', 'application/json')
+
+    xhr.onreadystatechange = function (){
+        if (xhr.readyState == 4){
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
+    };
+
+    let name = document.getElementById('articleNameInput').value;
+    let body = document.getElementById('articleBodyInput').value;
+    let date = new Date();
+
+    data = `{
+        "article-name": ${name},
+        "article-body": ${body},
+        "date": ${date}
+    }`;
+
+    xhr.send(JSON.stringify(data));
+}
+
+async function putWithFetch(){
+    let name = document.getElementById('articleNameInput').value;
+    let body = document.getElementById('articleBodyInput').value;
+    let date = new Date();
+
+    let data = `{
+        "article-name": ${name},
+        "article-body": ${body},
+        "date": ${date}
+    }`;
+
+    fetch("https://httpbin.org/put", {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {console.log(JSON.stringify(data))});
+
 }
 
 
@@ -115,5 +167,21 @@ function put(){
 
 
 function deleteRequest(){
-    
+    let deleteMethod = document.getElementsByName('deleteMethod');
+
+    if(deleteMethod[0].checked){
+        deleteWithXML();
+    } else if(deleteMethod[1].checked){
+        deleteWithFetch();
+    } else{
+        document.getElementById('response').innerText = "Please select a DELETE method";
+    }
+}
+
+function deleteWithXML(){
+    alert("in deleteWithXML");
+}
+
+function deleteWithFetch(){
+    alert("in deleteWithFetch");
 }
