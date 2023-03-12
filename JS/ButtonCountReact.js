c32175740f937d3ffbe4548b8cf2578cf769199f
HaitongC <haitongchen116@gmail.com>
2023-03-11 21:55:51 -0800

    ex react done

--- JS/ButtonCountReact.js
+++ JS/ButtonCountReact.js
@@ -1,8 +1,25 @@
-class ButtonCountReact extends HTMLElement{
-    connectedCallback() {
-        const btn = document.createElement('button');
-        this.attachShadow({mode: 'open'}).appendChild(btn);
+class ButtonCountReact extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
     }
+    
+    increment(){
+        this.setState((prev) => ({
+            count: prev.count + 1
+        }));
+    }
+
+    render() {
+        return(
+        <>
+            <h2>React Web Component Counter</h2>
+            <button onClick={() => this.increment()}>Times Clicked: {this.state.count}</button>
+        </>
+    )}
 }
 
-customElements.define('button-count-react', ButtonCountReact);
+ReactDOM.render(<ButtonCountReact />, document.getElementById('root'));
